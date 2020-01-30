60bf60f179202b00c79ad05288f6e910161d59ea
theJollySin <john_science@protonmail.com>
2020-01-30 06:21:50 -0500

    Project: Added spatial struct

--- little_project/main.cpp
+++ little_project/main.cpp
@@ -1,4 +1,4 @@
-// g++ main.cpp --std=c++14 -o stellar_dynamics.exe
+// g++ main.cpp --std=c++14 -o stellar.exe
 #include <iostream>
 #include <math.h>
 
@@ -10,12 +10,20 @@ const double earth_radius{ 6378000.0 };     // m
 const double sun_mass{ 1.32712440018e20 };  // kg
 
 
-struct particle {
-  double mass{ 100.0 };         // kg
-  double position[3] = {0.0, 0.0, 0.0};  // m
-  double velocity[3] = {0.0, 0.0, 0.0};;  // m/s
-  double radius{ 1.0 };       // m
+struct spatial {
+  double x { 0 };
+  double y { 0 };
+  double z { 0 };
 };
+
+
+struct particle {
+  double mass{ 100.0 };  // kg
+  double radius{ 1.0 };  // m
+  spatial position;   // m
+  spatial velocity;   // m/s
+};
+
 
 
 double graviational_force(double m1, double m2, double distance)
@@ -32,7 +40,31 @@ double velocity_from_force(double force, double time_delta, double m)
 
 double distance_two_points(particle p1, particle p2)
 {
-    return sqrt(pow(p2.position[0] - p1.position[0], 2) + pow(p2.position[1] - p1.position[1], 2) + pow(p2.position[2] - p1.position[2], 2));
+    return sqrt(pow(p2.position.x - p1.position.x, 2) + pow(p2.position.y - p1.position.y, 2) + pow(p2.position.z - p1.position.z, 2));
+}
+
+
+spatial get_direction(particle p1, particle p2)
+{
+    double dist { distance_two_points(p1, p2) };
+    spatial direction;
+
+    direction.x = (p2.position.x - p1.position.x) / dist;
+    direction.y = (p2.position.y - p1.position.y) / dist;
+    direction.z = (p2.position.z - p1.position.z) / dist;
+
+    return direction;
+}
+
+
+void update_particle(particle p, spatial direction, double velocity, double time_delta) {
+    p.velocity.x += velocity * direction.x;
+    p.velocity.y += velocity * direction.y;
+    p.velocity.z += velocity * direction.z;
+
+    p.position.x += velocity * direction.x * time_delta;
+    p.position.y += velocity * direction.y * time_delta;
+    p.position.z += velocity * direction.z * time_delta;
 }
 
 
@@ -45,8 +77,8 @@ int main()
     particle satelite;
     satelite.mass = 1000;
     satelite.radius = 5;
-    satelite.position[0] = 24e6;
-    satelite.velocity[1] = 15e6;
+    satelite.position.x = 24e6;
+    satelite.velocity.y = 15e6;
 
     std::cout << "Distance from Earth to the satelite: " << distance_two_points(earth, satelite) - earth.radius - satelite.radius << "\n";
 
