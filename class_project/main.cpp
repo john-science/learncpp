ecb4c21c1b37a498d7bf06cdd839e25e2a245104
theJollySin <john_science@protonmail.com>
2020-02-02 19:14:58 -0500

    Progress - Hubble has a stable orbit. But it's too eccentric.

--- class_project/main.cpp
+++ class_project/main.cpp
@@ -5,7 +5,7 @@
 const double pi{ 3.1415926535897932384626 };
 const double G{ 6.6743e-11 };               // m**3 / (kg * s)
 const double C{ 299792458 };                // m / s
-const double earth_mass{ 3.986004418e14 };  // kg
+const double earth_mass{ 5.9722e24 };  // kg
 const double earth_radius{ 6378000.0 };     // m
 const double sun_mass{ 1.32712440018e20 };  // kg
 
@@ -25,15 +25,15 @@ typedef struct particle {
 } particle;
 
 
-double gravitational_force(double m1, double m2, double dist)
+double gravitational_force(double mass1, double mass2, double dist)
 {
-    return G * m1 * m2 / (dist * dist);
+    return G * mass1 * mass2 / (dist * dist);
 }
 
 
-double velocity_from_force(double force, int time_delta, double m)
+double velocity_from_force(double force, int time_delta, double mass)
 {
-    return (force * time_delta) / m;
+    return (force * time_delta) / mass;
 }
 
 
@@ -45,12 +45,18 @@ double distance(particle p1, particle p2)
 
 spatial get_direction(particle p1, particle p2)
 {
-    double dist { distance(p1, p2) };
+    double total{ 0.0 };
     spatial direction;
 
-    direction.x = (p2.position.x - p1.position.x) / dist;
-    direction.y = (p2.position.y - p1.position.y) / dist;
-    direction.z = (p2.position.z - p1.position.z) / dist;
+    direction.x = p2.position.x - p1.position.x;
+    direction.y = p2.position.y - p1.position.y;
+    direction.z = p2.position.z - p1.position.z;
+
+    total = abs(direction.x) + abs(direction.y) + abs(direction.z);
+
+    direction.x /= total;
+    direction.y /= total;
+    direction.z /= total;
 
     return direction;
 }
@@ -72,16 +78,20 @@ void update_universe(particle *p1, particle *p2, int time_delta) {
     double gforce{ gravitational_force(p1->mass, p2->mass, dist) };
     double v1{ velocity_from_force(gforce, time_delta, p1->mass) };
     double v2{ velocity_from_force(gforce, time_delta, p2->mass) };
+
+    //std::cout << "\ndist: " << dist << " gforce: " << gforce << " v1: " << v1 << " v2: " << v2 << "\n";
+
     spatial direction1to2{ get_direction(*p1, *p2) };
-    std::cout << "direction1to2 " << direction1to2.x << " " << direction1to2.y << " " << direction1to2.z << "\n";
     spatial direction2to1;
     direction2to1.x = -direction1to2.x;
     direction2to1.y = -direction1to2.y;
     direction2to1.z = -direction1to2.z;
-    std::cout << "direction2to1 " << direction2to1.x << " " << direction2to1.y << " " << direction2to1.z << "\n";
 
-    update_particle(p1, direction2to1, v1, time_delta);
-    update_particle(p2, direction1to2, v2, time_delta);
+    //std::cout << "direction1to2 " << direction1to2.x << " " << direction1to2.y << " " << direction1to2.z << "\n";
+    //std::cout << "direction2to1 " << direction2to1.x << " " << direction2to1.y << " " << direction2to1.z << "\n";
+
+    update_particle(p1, direction1to2, v1, time_delta);
+    update_particle(p2, direction2to1, v2, time_delta);
 
 }
 
@@ -90,30 +100,40 @@ void update_universe(particle *p1, particle *p2, int time_delta) {
 int main()
 {
     double dist{ 1 };
+    int t{ 0 };
+    int dt{ 60 };
 
     // Init Earth
     particle earth;
     earth.mass = earth_mass;
     earth.radius = earth_radius;
 
-    // Init Satelite
+    // Init Satelite (Hubble)
     particle satelite;
-    satelite.mass = 100;
-    satelite.radius = 10;
-    satelite.position.x = 24e6;
-    satelite.velocity.y = 15e6;
+    satelite.mass = 11110;
+    satelite.radius = 8.2;
+    satelite.position.x = 6.9171e6;
+    //satelite.velocity.y = 7.5911e3;
+    satelite.velocity.y = sqrt(G * earth_mass / satelite.position.x) * 1.001;
 
-    for (int t=0; t < 2; t++) {
-        update_universe(&earth, &satelite, 60 * 60);
+    while(t < 14 * 24 * 60 * 60) {
+    //while(t < 4 * 60 * 60) {
+    //while(t < 6) {
+        update_universe(&earth, &satelite, dt);
 
         dist = distance(earth, satelite);
-        if (dist <= (earth.radius + satelite.radius)){
+        if (std::abs(dist) <= (earth.radius + satelite.radius)){
             std::cout << "BOOM! Collision at time " << t << "\n";
+            std::cout << "\tsatelite: (" << satelite.position.x << ", " << satelite.position.y << ", " << satelite.position.z << ")\n";
+            std::cout << "\tearth: (" << earth.position.x << ", " << earth.position.y << ", " << earth.position.z << ")\n";
             break;
-        } else if (t % 10017 == 0) {
-            std::cout << "\nThe satellite is " << dist<< " meters from Earth at time " << t << ".\n";
-            std::cout << "\t" << satelite.position.x << ", " << satelite.position.y << ", " << satelite.position.z << " -- " << gravitational_force(earth.mass, satelite.mass, dist) << "\n";
+        } else if ((t % (12 * 60 * 60)) == 0) {
+            std::cout << "The satellite is  " << dist<< "  m from Earth at time " << (t / (24 * 60 * 60)) << ".\n";
+            //std::cout << "\tsatelite: (" << satelite.position.x << ", " << satelite.position.y << ", " << satelite.position.z << ")\n";
+            //std::cout << "\tearth: (" << earth.position.x << ", " << earth.position.y << ", " << earth.position.z << ")\n";
         }
+
+        t += dt;
     }
 
     return 0;
