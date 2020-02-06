2954ac0bff68ed0f14adc2d749ddfbc16ffc9095
John Stilley <1831479+theJollySin@users.noreply.github.com>
2020-02-06 08:48:40 -0500

    Moving our data to a data-specific file

---
+++ class_project/2_three_body_problem/scientific_data.h
@@ -0,0 +1,20 @@
+
+#if !defined(SCIENCE_DATA_H)
+#define SCIENCE_DATA_H 1
+
+/* scientific constants */
+const double pi{ 3.1415926535897932384626 };
+const double G{ 6.6743e-11 };               // m**3 / (kg * s)
+const double c{ 299792458 };                // m / s
+
+/* astronomical data */
+const double hubble_mass{ 11110 };          // kg
+const double hubble_radius{ 8.2 };          // m
+const double moon_mass{ 7.342e22 };         // kg
+const double moon_radius{ 1.7374e6 };       // m
+const double earth_mass{ 5.9722e24 };       // kg
+const double earth_radius{ 6.378e6 };       // m
+const double sun_mass{ 1.9885e30 };         // kg
+const double sun_radius{ 6.96342e8 };       // m
+
+#endif
