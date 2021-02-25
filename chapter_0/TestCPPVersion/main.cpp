bd443441939da4507e38223cc51f6c9b00770603
theJollySin <john_science@protonmail.com>
2021-02-24 21:24:09 -0500

    Whitespace cleanup

--- chapter_0/TestCPPVersion/main.cpp
+++ chapter_0/TestCPPVersion/main.cpp
@@ -9,11 +9,13 @@ namespace a::b::c
     inline constexpr std::string_view str{ "hello" };
 }
 
+
 template <class... T>
 std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args)
 {
     return { sizeof...(T), (args + ...) };
 }
+
 
 int main()
 {
