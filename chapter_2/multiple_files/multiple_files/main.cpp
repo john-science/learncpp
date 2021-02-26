b716c8e429379d4493f37b64c8afc681dd96ea6f
theJollySin <john_science@protonmail.com>
2021-02-25 20:00:00 -0500

    Preprocessor example

--- chapter_2/multiple_files/multiple_files/main.cpp
+++ chapter_2/multiple_files/multiple_files/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "add.h"
 
+#define SOME_TEXT "The sum of "
+
 
 int main()
 {
-    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+    std::cout << SOME_TEXT << "3 and 4 is: " << add(3, 4) << '\n';
     return 0;
 }
