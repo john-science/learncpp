1f3bc933d077cf65b6839386c6deede7eff8088f
theJollySin <john_science@protonmail.com>
2021-03-03 20:17:47 -0500

    ARRAYS - still the easy stuff

--- chapter_9/arraysAndStrings/main.cpp
+++ chapter_9/arraysAndStrings/main.cpp
@@ -2,17 +2,37 @@
 
 using namespace std;
 
+enum StudentNames
+{
+    kenny, // 0
+    kyle, // 1
+    stan, // 2
+    butters, // 3
+    cartman, // 4
+    wendy, // 5
+    max_students // 6
+};
+
+
 int main()
 {
-    int prime[5]{}; // hold the first 5 prime numbers
-    prime[0] = 2; // The first element has index 0
-    prime[1] = 3;
-    prime[2] = 5;
-    prime[3] = 7;
-    prime[4] = 11; // The last element has index 4 (array length-1)
+    std::cout << "If we don't fully initialize an array, the g++ standard is to insert zeros.\n";
+    std::cout << "I hate this, let's just explicitly initialize our arrays.\n";
+    int array[5]{ 7, 4, 5 }; // only initialize first 3 elements
 
-    std::cout << "The lowest prime number is: " << prime[0] << '\n';
-    std::cout << "The sum of the first 5 primes is: " << prime[0] + prime[1] + prime[2] + prime[3] + prime[4] << '\n';
+    std::cout << array[0] << '\n';
+    std::cout << array[1] << '\n';
+    std::cout << array[2] << '\n';
+    std::cout << array[3] << '\n';
+    std::cout << array[4] << '\n';
+
+    std::cout << "\nWe can use enums to make indexing our arrays more meaningful.\n";
+    int testScores[max_students]{}; // allocate 6 integers
+    testScores[stan] = 76;
+
+    std::cout << testScores[stan] << '\n';
+
+
 
     return 0;
 }
